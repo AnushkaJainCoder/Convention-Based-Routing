392d5a6cdad8e675a996234471a273515cba93c8
anushka.jain <anushka.jain@innovasolutions.com>
2026-01-30 22:12:22 +0530

    razorview engine

--- obj/Debug/net8.0/MiddlewareApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MiddlewareApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiddlewareApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00ea835c94e02147b73d8232d6a445526cea1111")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e64d484ea50013ce6a32129ea0e575f10b4d4c9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiddlewareApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiddlewareApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
